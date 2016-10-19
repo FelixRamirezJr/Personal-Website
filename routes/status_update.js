f375efc7e701b0f8a9f54b21d727f6fbf9ef6b7b
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2016-10-19 15:28:45 -0700

    Uploading Changes...

--- routes/status_update.js
+++ routes/status_update.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/status_update', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('status_update');
 });
 
