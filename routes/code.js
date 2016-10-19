3ede4a5682f0b76aa7536594e8fe41af780583c8
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2016-10-19 15:47:17 -0700

    Uploading Changes...

--- routes/code.js
+++ routes/code.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+// Connect to Server
+var mongoose = require ("mongoose"); // The reason for this demo.
+// Connecting To Server
+var mongoUri = process.env.MONGODB_URI;
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoUri, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + mongoUri + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + mongoUri);
+      }
+});
+var ContactSchema = new mongoose.Schema({
+      name: String,
+      message: String,
+      email: String,
+      receipt: String,
+      status: String
+});
 
 /* GET me page. */
-var sendgrid = require('sendgrid')("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");
 router.get('/', function(req, res, next) {
   var code = req.query.info.toString();
   var Cont = mongoose.model('Contacts', ContactSchema);
