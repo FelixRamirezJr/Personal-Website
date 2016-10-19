294647187bbcacd44229a95b74fc133e633f6da5
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2016-10-19 16:54:12 -0700

    Uploading Changes...

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-    server.listen(process.env.PORT || 3000);
-
     // Required for Heroku Cloud Server
     io.on('connection', function () {
       io.set("transports", ["xhr-polling"]);
