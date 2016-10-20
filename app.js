5927fa4f980b0bfe6d12802c5e3e5146ef35e9c3
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2016-10-19 17:00:46 -0700

    Uploading Changes...

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
     io = require('socket.io').listen(server);
     // Required for Heroku Cloud Server
     io.on('connection', function () {
